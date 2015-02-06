14799576b962362762dc6cf4bf8ad9273e49f593
ajays20078 <ajays20078@gmail.com>
2015-02-06 17:06:14 +0530

    http-logger

--- README.md
+++ README.md
@@ -1,20 +1,5 @@
 # go-http-logger
 
+WriteLog Logs the Http Status for a request into fileHandler
+Syntax : http.ListenAndServe(Virtual_Host+":"+port, httpLogger.WriteLog(http.DefaultServeMux,fileHandler))
 
-
-go-http-logger is a asynchronous non-blocking logger for go HTTP Server which logs the requests in the below format
-
-**%YYYY/MM/DD% %HH:MM:SS% %VirtualHost:Port% %ClientHost:Port% "%HttpMethod% %URL?Paramaters% %HttpVersion%" %HttpReturnCode% %ResponseByteSize% "%HttpClient%" %ResponseTime%**
-
-Where ResponseTime is in MilliSeconds
-
-
-
-It takes the following arguments:  <br/>
-
-* request - HTTP request from the client  <br/>
-* return_status - HTTP return status from the server  <br/>
-* return_string - HTTP response string  <br/>
-* file_handler - File to which Log has to be written to  <br/>
-* start_time - localtime in Nanoseconds (You can use time.Now().UnixNano()) when the HTTP handler starts its execution  <br/>
-* end_time - localtime in Nanoseconds (You can use time.Now().UnixNano()) when the HTTP handler finishes its execution  <br/>
--- httpLogger.go
+++ httpLogger.go
@@ -4,27 +4,45 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strconv"
+	"time"
 )
 
-// LogLine function calls logLineAsync as a goroutine to Log to Disk Asynchronously and returns immediately so that requests are not blocked on Disk Write.
-// It take http request, return status ,return string ,filehandler to log to, start time and end time of the request as arguments.
-func LogLine(request *http.Request, returnStatus int, returnString string, fileHandler *os.File, startTime int64, endTime int64) int {
-	go logLineAsync(request, returnStatus, returnString, fileHandler, startTime, endTime)
-	return 0
+
+type statusWriter struct {
+	http.ResponseWriter
+	status int
+	length int
 }
 
+func (w *statusWriter) WriteHeader(status int) {
+	w.status = status
+	w.ResponseWriter.WriteHeader(status)
+}
 
-//logLineAsync logs to disk asynchronously.
-func logLineAsync(request *http.Request, returnStatus int, returnString string, fileHandler *os.File, startTime int64, endTime int64) {
-	timeDiff := (float64(endTime) - float64(startTime)) / 1000000.0
-	timeTakenString := strconv.FormatFloat(timeDiff, 'f', 3, 64)
-	log.SetOutput(fileHandler)
-	httpReturnStatus := strconv.FormatInt(int64(returnStatus), 10)
-	responseLength := strconv.FormatInt(int64(len(returnString)), 10)
-	if request.URL.RawQuery != "" {
-		log.Println(request.Host + " " + request.RemoteAddr + " \"" + request.Method + " " + request.URL.Path + "?" + request.URL.RawQuery + " " + request.Proto + "\"" + " " + httpReturnStatus + " " + responseLength + " " + "\"" + request.Header.Get("User-Agent") + "\"" + " " + timeTakenString)
-	} else {
-		log.Println(request.Host + " " + request.RemoteAddr + " \"" + request.Method + " " + request.URL.Path + " " + request.Proto + "\"" + " " + httpReturnStatus + " " + responseLength + " " + "\"" + request.Header.Get("User-Agent") + "\"" + " " + timeTakenString)
+func (w *statusWriter) Write(b []byte) (int, error) {
+	if w.status == 0 {
+		w.status = 200
+	}
+	w.length = len(b)
+	return w.ResponseWriter.Write(b)
+}
+
+// WriteLog Logs the Http Status for a request into fileHandler
+// Syntax : http.ListenAndServe(Virtual_Host+":"+port, httpLogger.WriteLog(http.DefaultServeMux,fileHandler))
+func WriteLog(handle http.Handler,fileHandler *os.File) http.HandlerFunc {
+	logger := log.New(fileHandler, "", 0)
+	return func(w http.ResponseWriter, request *http.Request) {
+		start := time.Now()
+		writer := statusWriter{w, 0, 0}
+		handle.ServeHTTP(&writer, request)
+		end := time.Now()
+		latency := end.Sub(start)
+		statusCode := writer.status
+		length := writer.length
+		if request.URL.RawQuery != "" {
+			logger.Printf("%v %s %s \"%s %s%s%s %s\" %d %d \"%s\" %v",end.Format("2006/01/02 - 15:04:05"),request.Host,request.RemoteAddr,request.Method,request.URL.Path,"?",request.URL.RawQuery,request.Proto,statusCode,length,request.Header.Get("User-Agent"),latency)
+		} else {
+			logger.Printf("%v %s %s \"%s %s %s\" %d %d \"%s\" %v",end.Format("2006/01/02 - 15:04:05"),request.Host,request.RemoteAddr,request.Method,request.URL.Path,request.Proto,statusCode,length,request.Header.Get("User-Agent"),latency)
+		}
 	}
 }
