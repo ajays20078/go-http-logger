93e5c8e6345da3a776e1b6cca970f28ee00ea510
ajays20078 <ajays20078@gmail.com>
2015-02-06 17:20:11 +0530

    fmt

--- httpLogger.go
+++ httpLogger.go
@@ -7,7 +7,6 @@ import (
 	"time"
 )
 
-
 type statusWriter struct {
 	http.ResponseWriter
 	status int
@@ -28,7 +27,7 @@ func (w *statusWriter) Write(b []byte) (int, error) {
 }
 
 // WriteLog Logs the Http Status for a request into fileHandler and returns a httphandler function which is a wrapper to log the requests.
-func WriteLog(handle http.Handler,fileHandler *os.File) http.HandlerFunc {
+func WriteLog(handle http.Handler, fileHandler *os.File) http.HandlerFunc {
 	logger := log.New(fileHandler, "", 0)
 	return func(w http.ResponseWriter, request *http.Request) {
 		start := time.Now()
@@ -39,9 +38,9 @@ func WriteLog(handle http.Handler,fileHandler *os.File) http.HandlerFunc {
 		statusCode := writer.status
 		length := writer.length
 		if request.URL.RawQuery != "" {
-			logger.Printf("%v %s %s \"%s %s%s%s %s\" %d %d \"%s\" %v",end.Format("2006/01/02 - 15:04:05"),request.Host,request.RemoteAddr,request.Method,request.URL.Path,"?",request.URL.RawQuery,request.Proto,statusCode,length,request.Header.Get("User-Agent"),latency)
+			logger.Printf("%v %s %s \"%s %s%s%s %s\" %d %d \"%s\" %v", end.Format("2006/01/02 - 15:04:05"), request.Host, request.RemoteAddr, request.Method, request.URL.Path, "?", request.URL.RawQuery, request.Proto, statusCode, length, request.Header.Get("User-Agent"), latency)
 		} else {
-			logger.Printf("%v %s %s \"%s %s %s\" %d %d \"%s\" %v",end.Format("2006/01/02 - 15:04:05"),request.Host,request.RemoteAddr,request.Method,request.URL.Path,request.Proto,statusCode,length,request.Header.Get("User-Agent"),latency)
+			logger.Printf("%v %s %s \"%s %s %s\" %d %d \"%s\" %v", end.Format("2006/01/02 - 15:04:05"), request.Host, request.RemoteAddr, request.Method, request.URL.Path, request.Proto, statusCode, length, request.Header.Get("User-Agent"), latency)
 		}
 	}
 }
